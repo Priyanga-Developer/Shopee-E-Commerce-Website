64d51abe0496d5155e1c4178a08ff76f10006214
Priyanga <uthirapriyanga@gmail.com>
2023-11-21 16:16:21 +0530

    added API
    added API

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ if(icon){
 
 /**Sign-in Animation***/
 
- const signInbtn =document.querySelector("#sign-in-btn");
- const signUpbtn =document.querySelector("#sign-up-btn");
+const signInbtn =document.querySelector("#sign-in-btn");
+const signUpbtn =document.querySelector("#sign-up-btn");
 const container=document.querySelector(".container");
 const signUpbtn2=document.querySelector("#sign-up-btn2");
 const signInbtn2=document.querySelector("#sign-in-btn2");
@@ -295,11 +295,73 @@ let dots=document.querySelectorAll(".dot");
 
 
 let shop=document.querySelector("#shop");
+const api ="https://fakestoreapi.com/products";
 
-let basket= JSON.parse(localStorage.getItem("itemsnumber")) || [];
+let basket = JSON.parse(localStorage.getItem("itemsnumber"))||[]; //Store the data which item we have selected{id:"",item:""}
+
+
+
+
+const getData= async()=>{
+  try{
+    const response =await fetch(api);
+    const data =await response.json();
+    return data;
+  
+  }
+  catch(err){
+    console.log(err.message)
+  }
+ 
+}
+let generateShop= async()=>{
+  try{
+    const result =await getData();
+    console.log(result)
+  return (shop.innerHTML=  result.map((x)=>{ 
+       
+
+   let {id,category,title,price,image}=x;
+   let search = basket.find((x)=>x.id==id)||[]
+
+   
+   return `<div class="pro" id="product-id-${id}"  data-filter="${category}">
+<div class="img-area">
+<img class="product-image" src=${image} alt="image">
+</div>
+<div class="des">
+ <div class="quantity-box">
+ <span class="category">${category}</span>
+ <div class="buttons">
+ <i onclick="decrement(${id})"class="fa-solid fa-minus"></i>
+ <div id=${id} class="quantity">
+${search.item===undefined?0:search.item}
+ </div>
+  <i onclick="increment(${id})"class="fa-solid fa-plus"></i>
+ </div>
+</div>
+ <h5 id="name">${title}</h5>
+ <h4>Rs.${price}</h4>
+ <div id="cart">
+   <a href="#">  <i onclick="increment(${id})" class="fa-solid fa-cart-arrow-down"></i></a>
+ </div>
+</div>
+</div>`;
+
+ }).join(""));
+  }
+
+  catch(err){
+    console.log(err.message)
+  }
+   
+    
+};
+generateShop();
+document.addEventListener ("DOMContentLoaded",getData());
+
+
 const iconCart=document.querySelectorAll("#cart-open");
-console.log(iconCart);
-
 if(basket.length>0){
   iconCart.forEach(eachicon => {
     eachicon .classList.add("bx-tada");
@@ -307,379 +369,60 @@ if(basket.length>0){
 
 }
 
-let calculation=()=>{
-  let cartIcon =document.querySelectorAll(".cartAmount");
- cartIcon.forEach(eachicon => {
-  eachicon.innerHTML=basket.map((x)=>x.item).reduce((x,y)=>x+y,0);
-  });
- 
-};
-
-calculation();
-
-
-  let shopItemsData=[{
-    id: "1",
-    category:"Mens",
-    name:"T-Shirt for Men",
-    price:781,
-    img:"./mens/t1.png",
-  },
-  { 
-  id:"2",
-  category:"Mens",
-  name:"T-Shirt for Men",
-  price:708,
-  img:"./mens/t2.png",
-  
-  },
-  { 
-  id:"3",
-  category:"Mens",
-  name:"T-Shirt for Men",
-  price:718,
-  img:"./mens/t3.png",
-  
-    
-  },
-  {
-    id:"4",
-    category:"Mens",
-    name:"T-Shirt for Men",
-    price:278,
-    img:"./mens/t4.png",
-    
-  },{
-    id:"5",
-    category:"Mens",
-    name:"T-Shirt for Men",
-    price:178,
-    img:"./mens/t5.png",
-    
-  },
-  {
-    id:"6",
-    category:"Mens",
-    name:"T-Shirt for Men",
-    price:781,
-    img:"./mens/t6.png",
-    
-  },
-  {
-    id:"7",
-    category:"Mens",
-    name:"Fashion Shirt for Men",
-    price:768,
-    img:"./mens/f1.jpg",
-    
-  },
-  {
-    id:"8",
-    category:"Mens",
-    name:"Fashion Shirt for Men",
-    price:787,
-    img:"./mens/f2.jpg",
-  },
-  {
-    id:"9",
-    category:"Mens",
-    name:"Fashion Shirt for Men",
-    price:787,
-    img:"./mens/f3.jpg",
-  },
-  {
-    id:"10",
-    category:"Mens",
-    name:"Fashion Shirt for Men",
-    price:787,
-    img:"./mens/f4.jpg",
-  },
-  {
-    id:"11",
-    category:"Mens",
-    name:"Fashion Shirt for Men",
-    price:787,
-    img:"./mens/f5.jpg",
-  },
-  {
-    id:"12",
-    category:"Mens",
-    name:"Fashion Shirt for Men",
-    price:787,
-    img:"./mens/shirt4.jpg",
-  },
-  {
-    id:"12",
-    category:"Women",
-    name:"Ethnic Top",
-    price:481,
-    img:"./womens/top1.jpg",
-  },
-  {
-    id:"13",
-    category:"Women",
-    name:"Printed Ethnic Top",
-    price:499,
-    img:"./womens/top2.jpg",
-  },
-  {
-    id:"14",
-    category:"Women",
-    name:"Embroidered Top",
-    price:639,
-    img:"./womens/top3.jpg",
-  },
-  {
-    id:"15",
-    category:"Women",
-    name:"Embroidered A-Line Top",
-    price:639,
-    img:"./womens/top4.jpg",
-  },
-  {
-    id:"16",
-    category:"Women",
-    name:"Anarkali Kurta",
-    price:549,
-    img:"./womens/kurti1.jpg",
-  },
-  {
-    id:"17",
-    category:"Women",
-    name:"Printed Striped Kurta",
-    price:649,
-    img:"./womens/kurti3.jpg",
-  },
-
-  {
-    id:"18",
-    category:"Women",
-    name:"Printed Top",
-    price:449,
-    img:"./womens/Printedtop.jpeg",
-  },
-  
-
-  
-  {
-    id:"19",
-    category:"Women",
-    name:"Pure Cotton Kurta",
-    price:449,
-    img:"./womens/kurti3.jpg",
-  },
-
-
-  ];
-
-
-
-
-  const mensCollection= document.querySelector(".Men");
-  const womenCollection=document.querySelector(".Women");
-  const allCollection=document.querySelector(".All");
-
- /**Filter by category**/
-
-
-
-if( allCollection){
-  allCollection.addEventListener("click",()=>{
-  
-   generateShop();
-  })
-
-}
-
-if(shop.classList.contains("index")){
-
-  shopItemsData=[{
-    id: "1",
-    category:"Mens",
-    name:"Fashion Shirt for Men",
-    price:781,
-    img:"./mens/shirt1.jpg",
-  },
-  { 
-  id:"2",
-  category:"Mens",
-  name:"Fashion Shirt for Men",
-  price:708,
-  img:"./mens/shirt2.jpg",
-  
-  },
-  { 
-  id:"3",
-  category:"Mens",
-  name:"Fashion Shirt for Men",
-  price:718,
-  img:"./mens/shirt3.jpg",
-  
-    
-  },
-  {
-    id:"4",
-    category:"Women",
-    name:"Fashion Shirt for MEN",
-    price:278,
-    img:"./mens/shirt4.jpg",
-    
-  }]
-
-}
-
-  let generateShop=()=>{
-     return (shop.innerHTML=  shopItemsData.map((x)=>{ 
-      let {id,category,name,price,img}=x;
-      let search =basket.find((x)=>x.newId==id) || [];
-      
-      return `<div class="pro" id="product-id-${id}" >
-<div class="img-area">
-  <img class="product-image" src=${img} alt="image">
-</div>
-  <div class="des">
-    <div class="quantity-box">
-    <span class="category">${category}</span>
-    <div class="buttons">
-    <i onclick="decrement(${id})"class="fa-solid fa-minus"></i>
-    <div id=${id} class="quantity">
-    ${search.item === undefined ? 0 :search.item}
-    </div>
-     <i onclick="increment(${id})"class="fa-solid fa-plus"></i>
-    </div>
-  </div>
-    <h5 id="name">${name}</h5>
-    <h4>Rs.${price}</h4>
-    <div id="cart">
-      <a href="#">  <i onclick="increment(${id})" class="fa-solid fa-cart-arrow-down"></i></a>
-    </div>
-  </div>
-</div>`;
-
-    }).join(""));
-       
-  };
-  generateShop(); 
-  
- 
-
-  if( document.querySelector(".search")){
-    document.querySelector(".search").addEventListener("click",()=>{
-      let searchinput=document.getElementById("inputfield").value;
-      let elements =document.querySelectorAll("#name");
-  
-      let cards=document.querySelectorAll(".pro");
-      elements.forEach((element,index) => {
-        let textContent=element.textContent.toUpperCase();
-        console.log(textContent);
-        if(textContent.includes(searchinput.toUpperCase()))
-        {
-          cards[index].classList.remove("hide");
-        }
-  
-        else{
-          cards[index].classList.add("hide");
-        }
-       
-      });
-  
-     
-      });
-  }
- 
-
-
-
-
-
-let filterProduct=(value)=>{
-  const selectCategoryBtn=document.querySelectorAll(".button-value");
-  selectCategoryBtn.forEach((btns)=>{
-
-    let element=document.querySelectorAll(".category");
-  let cards=document.querySelectorAll(".pro");
-  element.forEach((element,index)=>{
-    let textContent=element.textContent.toUpperCase();
-    if(value.toUpperCase()&&btns.innerText.toUpperCase()=="ALL"){
-      generateShop(); 
-    }
-      
-     if (value.toUpperCase()==btns.innerText.toUpperCase()){
-      btns.classList.add("button-active");
-      if(textContent.includes(value.toUpperCase())){
-       cards[index].classList.remove("hide");
-     
-     }
-     else{
-       cards[index].classList.add("hide");
-     }
-    }  
-    
-    else{
-     btns.classList.remove("button-active");
-    }
- 
-  })
-
- 
-  })
-
-}
-
-window.onload=()=>{
-  filterProduct("all");
-}
-
-
-
-  let increment=(id)=>{
-  let selectedId=id;
-  let search=basket.find ((x)=>x.newId===selectedId);
+let increment= (id)=>{
   iconCart.forEach(eachicon => {
-    eachicon.classList.add("bx-tada");
+    eachicon .classList.add("bx-tada");
   });
 
-  if(search===undefined){
+  let selectedItem =id;
+  let search = basket.find((x)=>x.id===selectedItem);
+  if(search ===undefined){
     basket.push({
-      newId:selectedId,
+      id:selectedItem,
       item:1,
-    });
-   
+    })
   }
   else{
     search.item+=1;
   }
-
-  update(selectedId);
+  update(selectedItem);
   localStorage.setItem("itemsnumber",JSON.stringify(basket));
+}
 
-};
-
-let decrement =(id)=>{
- 
-  let selectedId=id;
-  let search=basket.find ((x)=>x.newId===selectedId);
-
-  if(search === undefined) return;
-  else if(search.item===0)return;
+let decrement= (id)=>{
+  let selectedItem =id;
+  let search = basket.find((x)=>x.id===selectedItem);
+  if(search===undefined) return
+  else if(search.item ===0) return; // item becomes 0 then function stops
   else{
-    search.item -= 1;
+    search.item -=1;
   }
- 
-  update(selectedId);
-  basket = basket.filter((x) => x.item !== 0);
+
+  update(selectedItem);
+  basket=basket.filter((x)=>x.item!==0);//if item is 0,then whole object deleted
+
   localStorage.setItem("itemsnumber",JSON.stringify(basket));
- 
-};
+}
 
-let update=(id)=>{ 
-  let search = basket.find((x)=>x.newId ===id);
-    document.getElementById(id).innerHTML=search.item;
-    calculation();
-};
-
+// calculating the items number and displaying in div
+let update= (id)=>{
+  let search =basket.find((x)=>x.id===id)
+  document.getElementById(id).innerHTML=search.item;
+  calculation();
+}
+// total items to be displayed in notification  bar
+let calculation =()=>{
+  let cartIcon =document.querySelectorAll(".cartAmount");
+  cartIcon.forEach(eachicon => {
+    eachicon.innerHTML=basket.map((x)=>x.item).reduce((x,y)=>x+y,0);
+    });
+}
+calculation();
 
 const cartopen =document.querySelector("#cart-page");
+
+// Opening cart page
+
 iconCart.forEach(eachicon => {
   eachicon.addEventListener("click",()=>{
     cartopen.style.right="0"
@@ -687,164 +430,152 @@ iconCart.forEach(eachicon => {
     generateCartItems();
     calculation();
     TotalAmount();
+    // window.location.reload();
   })
 });
 
-
-
-let goShop=()=>{
-  cartopen.style.right="100%";
-  window.location.reload("shop.html");
-}
-
-
-let cartclosebtn =document.querySelector(".text-center")
+// Back to Shop Page
 let closecart=()=>{
   cartopen.style.right="100%";
   window.location.reload("shop.html");
 }
 
-
-
-
-
+let cartclosebtn =document.querySelector(".text-center");
 let label=document.querySelector("#label");
 let ShoppingCart =document.querySelector("#shopping-cart");
 
-let generateCartItems=()=>{
+let generateCartItems=async()=>{
+   const result =await getData();
   if(basket.length!==0){
-     return (ShoppingCart.innerHTML=basket.map((x)=>{
-      let {newId , item}=x;
-      let search = shopItemsData.find((y)=>y.id==newId) || [];
-      let {img,name,price}=search;
-      return ` 
-      <div class= "cart-item">
+    return (ShoppingCart.innerHTML=basket.map((x)=>{
+       let {id,item} =x;
+       let search =result.find((y)=>y.id===id)|| [];
+       let {image,title,price}=search;
+       
+      return ` <div class= "cart-item">
       
-            <img width="100" src=${img} alt=""/>
-            <div class="details">
+      <img width="100" src=${image} alt=""/>
+      <div class="details">
 
-              <div class="title-price-x">
-              <h4 class="title-price">
-              <p>${name}</p>
-              <p class="cart-item-price">Rs.${price}</p>
-              </h4>
-              <i onclick="removeItem(${newId})"class="fa-solid fa-square-xmark" style="color: #000000;"></i>
-              </div>
-              <div class="buttons">
-                <i onclick="decrement1(${newId})"class="fa-solid fa-minus"></i>
-                <div id=${newId} class="quantity">${item}</div>
-                <i onclick="increment1(${newId})"class="fa-solid fa-plus"></i>
-              </div>
-            <h3>Rs.${item*price}</h3>
-          </div>
-            </div>
+        <div class="title-price-x">
+        <h4 class="title-price">
+        <p>${title}</p>
+        <p class="cart-item-price">Rs.${price}</p>
+        </h4>
+        <i onclick="removeItem(${id})"class="fa-solid fa-square-xmark" style="color: #000000;"></i>
+        </div>
+        <div class="buttons">
+          <i onclick="decrement1(${id})"class="fa-solid fa-minus"></i>
+          <div id=${id} class="quantity">${item}</div>
+          <i onclick="increment1(${id})"class="fa-solid fa-plus"></i>
+        </div>
+      <h3>Rs.${Math.floor(item*price)}</h3>
+    </div>
       </div>
-      `
-     })
-     .join(""));
+</div>`;
+    }).join(" "))
   }
   else{
     ShoppingCart.innerHTML=``;
     label.innerHTML=`     
     <i onclick ="closecart();"class="fa-solid fa-circle-xmark"></i>
     <span> The Cart is Empty</span>
-    <button class="HomeBtn" onclick="goShop();" id="backtoHome">Back to Shop</button>
-    ` 
-   
+    <button class="HomeBtn" onclick="goShop();" id="backtoHome">Back to Shop</button>`
+    
   }
 };
 
 generateCartItems();
 
-let increment1=(id)=>{
-  let selectedId=id;
 
-  let search=basket.find ((x)=>x.newId===selectedId);
-  if(search===undefined){
+let increment1= (id)=>{
+  let selectedItem =id;
+  let search = basket.find((x)=>x.id===selectedItem);
+  if(search ===undefined){
     basket.push({
-      newId:selectedId,
+      id:selectedItem,
       item:1,
-    });
-   
+    })
   }
   else{
     search.item+=1;
   }
   generateCartItems();
-  update1(selectedId);
+  update1(selectedItem);
   localStorage.setItem("itemsnumber",JSON.stringify(basket));
+}
 
-};
-
-let decrement1 =(id)=>{
- 
-  let selectedId=id;
-  let search=basket.find ((x)=>x.newId===selectedId);
-
-  if(search === undefined) return;
-  else if(search.item===0)return;
+let decrement1= (id)=>{
+  let selectedItem =id;
+  let search = basket.find((x)=>x.id===selectedItem);
+  if(search===undefined) return
+  else if(search.item ===0) return; // item becomes 0 then function stops
   else{
-    search.item -= 1;
+    search.item -=1;
   }
- 
-  update1(selectedId);
-  basket = basket.filter((x) => x.item !== 0);
+
+  update1(selectedItem);
+  basket=basket.filter((x)=>x.item!==0);//if item is 0,then whole object deleted
   generateCartItems();
   localStorage.setItem("itemsnumber",JSON.stringify(basket));
- 
-};
-
-let update1=(id)=>{ 
-  let search = basket.find((x)=>x.newId ===id);
-    document.getElementById(id).innerHTML=search.item;
-    calculation();
-    TotalAmount();
-};
-
-let removeItem=(id)=>{
-   let selectedItem =id;
-   console.log(basket)
-   basket=basket.filter((x)=>x.newId !== selectedItem);
-   generateCartItems();
-   TotalAmount();
-   calculation();
-   localStorage.setItem("itemsnumber",JSON.stringify(basket));
-   
 }
 
-let clearCart=()=>{
-  basket=[]
-  generateCartItems();
+// calculating the items number and displaying in div
+let update1= (id)=>{
+  let search =basket.find((x)=>x.id===id)
+  document.getElementById(id).innerHTML=search.item;
   calculation();
-  localStorage.setItem("itemsnumber",JSON.stringify(basket));
-
+  TotalAmount();
 }
 
-let TotalAmount=()=>{
+
+
+
+
+// total amount
+
+let TotalAmount= async()=>{
+  const result =await getData();
   if(basket.length!==0){
     let amount =basket.map((x)=>{
-      let {newId, item} =x;
-      let search = shopItemsData.find((y)=>y.id==newId) || [];
+      let {id, item} =x;
+      let search = result.find((y)=>y.id===id) || [];
       return item* search.price;
     }).reduce((x,y)=>x+y ,0);
     label.innerHTML=`
     <i onclick ="closecart();"class="fa-solid fa-circle-xmark"></i>
-    <span> Total Bill : Rs.${amount}</span>
+    <span> Total Bill : Rs.${Math.floor(amount)}</span>
     <button id="checkout" onclick="checkOut();">Check Out</button>
     <button onclick="clearCart();" id="removeAll"> Clear Cart</button>
     `;
-    // console.log(amount);
+  
   }
   else return;
 }
 TotalAmount();
 
+//Remove the item
 
+let removeItem=(id)=>{
+  let selectedItem =id;
+  basket=basket.filter((x)=>x.id!== selectedItem);
+  generateCartItems();
+  TotalAmount();
+  calculation();
+  localStorage.setItem("itemsnumber",JSON.stringify(basket));
+}
 
-
+// clear cart
+let clearCart=()=>{
+  basket=[]
+  generateCartItems();
+  calculation();
+  localStorage.setItem("itemsnumber",JSON.stringify(basket));
+}
+//  Check out
 let checkOut=()=>{
-  clearCart();
-  label.innerHTML=`
+ ShoppingCart.innerHTML=``;
+ label.innerHTML=`
   <div class="pop">
   <i class='bx bxl-shopify  bx-lg' id="icon" ></i>
  <div class="pop-text">
@@ -853,5 +584,80 @@ let checkOut=()=>{
  </div>
  <button class="HomeBtn" onclick="goShop();" id="backtoHome">Back to Home</button>
  </div>`;
+}
+
+// Back to Shop Page
+
+
+let goShop=()=>{
+  clearCart();
+  cartopen.style.right="100%";
+  window.location.reload("shop.html");
+}
+
+
+
+//search page
+
+
+if( document.querySelector(".search")){
+  document.querySelector(".search").addEventListener("click",()=>{
+    let searchinput=document.getElementById("inputfield").value;
+    let elements =document.querySelectorAll("#name");
+    let cards=document.querySelectorAll(".pro");
+    elements.forEach((element,index) => {
+      let textContent=element.textContent.toUpperCase();
+   
+      if(textContent.includes(searchinput.toUpperCase()))
+      {
+        cards[index].classList.remove("hide");
+      }
+
+      else{
+        cards[index].classList.add("hide");
+      }
+     
+    });
+    });
+}
+
+const selectCategoryBtn=document.querySelectorAll(".button-value");
+
+
+selectCategoryBtn.forEach((btn)=>{
+    btn.addEventListener("click",(e)=>{
+      e.preventDefault();
+      setActiveBtn(e);
+      let cards=document.querySelectorAll(".pro");
+      const btnfilter =e.target.dataset.filter;
+
+        cards.forEach((card)=>{
+
+          if(btnfilter==="All"){
+
+            card.classList.remove("hide");
+          }
+          else{
+            const cardfilter=card.dataset.filter;
+          
+             if(btnfilter===cardfilter){
+              card.classList.remove("hide");
+  
+             }
+             else{
+              card.classList.add("hide");
+          ;
+             }
+          } 
+         
+        })
+    })
+})
+
+let setActiveBtn =(e)=>{
+  selectCategoryBtn.forEach((btn)=>{
+    btn.classList.remove("button-active");
+  })
+  e.target.classList.add("button-active");
 }
 
